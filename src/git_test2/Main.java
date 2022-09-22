8eb92529fb694f76a39a8745ce604d445c842b46
Yu,yeonju <yeonju010504@gmail.com>
2022-09-23 03:45:26 +0900

    main 메소드 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,7 +1,10 @@
 package git_test2;
 
 public class Main {
-	public static void main(String[] args) {
-		new Hello().say();
-	}
+
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.say();
+    }
+
 }
